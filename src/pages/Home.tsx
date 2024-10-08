9c3c00e1562d1aff77dd03252a29fd2fc5cb5bfb
Roman Mendaliev <reacheight8@gmail.com>
2024-10-08 23:25:47 +0300

    only by frist name

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,7 +7,7 @@ const Home: React.FC = () => {
   return (
     <div className="min-h-screen bg-bg_color flex items-center justify-center">
       <div className="bg-section_bg_color p-8 rounded-lg shadow-2xl max-w-md w-full">
-        <h1 className="text-4xl font-bold mb-6 text-center text-accent_text_color">{firstName} {lastName}, welcome to spokystan</h1>
+        <h1 className="text-4xl font-bold mb-6 text-center text-accent_text_color">{firstName}, welcome to spokystan</h1>
         <p className="text-subtitle_text_color mb-8 text-center">everythin is spoky here</p>
         
         <button 
