96e03fcdf5426a8bb9b09ea9649817d6b6e45dae
Roman Mendaliev <reacheight8@gmail.com>
2024-10-09 21:40:43 +0300

    keep playing

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -11,16 +11,11 @@ const Home: React.FC = () => {
       <h1 className="text-4xl font-bold mb-6 text-center text-accent_text_color">{firstName}, welcome to spokystan 😌</h1>
       <p className="text-subtitle_text_color mb-8 text-center">everythin' is spoky here</p>
       
-      {!wallet && (
-        <button className="w-full py-2 px-4 bg-button_color text-button_text_color rounded hover:opacity-90 transition duration-300">
-          connect wallet to become a spokystan citizen 🤙🏻
-        </button>
-      )}
-      {wallet && (
-        <div>
-          you are a spokystan citizen now
-        </div>
-      )}
+      <p className="text-center text-accent_text_color">
+        {wallet 
+          ? "🤙🏻 you are a spokystan citizen now"
+          : "👝 connect your wallet to become a spokystan citizen"}
+      </p>
     </div>
   );
 };
