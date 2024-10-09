23e547c26beb43b663da373c97d20f9da40fedd5
Roman Mendaliev <reacheight8@gmail.com>
2024-10-09 21:54:09 +0300

    keep playing

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,21 +1,24 @@
 import React from 'react';
 import { useAuth } from '../hooks/useAuth';
-import { useTonWallet } from '@tonconnect/ui-react';
+import { useIsConnectionRestored, useTonWallet } from '@tonconnect/ui-react';
 
 const Home: React.FC = () => {
   const { firstName } = useAuth()
   const wallet = useTonWallet()
+  const connectionRestored = useIsConnectionRestored();
 
   return (
     <div className="p-8 rounded-lg max-w-md w-full">
       <h1 className="text-4xl font-bold mb-6 text-center text-accent_text_color">{firstName}, welcome to spokystan 😌</h1>
       <p className="text-subtitle_text_color mb-8 text-center">everythin' is spoky here</p>
-      
-      <p className="text-center text-accent_text_color">
-        {wallet 
-          ? "🤙🏻 you are a spokystan citizen now"
-          : "👝 connect your wallet to become a spokystan citizen"}
-      </p>
+
+      {connectionRestored && (
+        <p className="text-center text-accent_text_color">
+          {wallet 
+            ? "🤙🏻 you are a spokystan citizen now"
+            : "👝 connect your wallet to become a spokystan citizen"}
+        </p>
+      )}
     </div>
   );
 };
