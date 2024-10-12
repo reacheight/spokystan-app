00fb78260965be2e9a4cbbfa7b8605e6a041b087
Roman Mendaliev <reacheight8@gmail.com>
2024-10-12 17:46:22 +0300

    use supabase to get all citizens

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useAuth } from '../hooks/useAuth';
 import { useIsConnectionRestored, useTonWallet } from '@tonconnect/ui-react';
+import { createClient } from "@supabase/supabase-js";
+
+const supabase = createClient(import.meta.env.VITE_SUPABASE_PROJECT_URL || process.env.SUPABASE_PROJECT_URL, import.meta.env.VITE_SUPABASE_ANON_KEY || process.env.SUPABASE_ANON_KEY)
 
 const Home: React.FC = () => {
   const { firstName } = useAuth()
   const wallet = useTonWallet()
   const connectionRestored = useIsConnectionRestored();
+
+  const [citizens, setCitizens] = useState<any[] | null>(null);
+
+  useEffect(() => {
+    getCitizens();
+  }, []);
+
+  const getCitizens = async () => {
+    const { data } = await supabase.from('citizens').select();
+    setCitizens(data || []);
+  }
 
   return (
     <div className="p-8 rounded-lg max-w-md w-full">
@@ -15,9 +29,26 @@ const Home: React.FC = () => {
       {connectionRestored && (
         <p className="text-center text-accent_text_color">
           {wallet 
-            ? "🤙🏻 you are a spokystan citizen now"
-            : "👝 connect your wallet to become a spokystan citizen"}
+            ? "🎖 you are a honorary citizen of spokystan"
+            : "👝 connect your wallet to become a citizen of spokystan"}
         </p>
+      )}
+
+      {citizens && (
+        <div className="mt-8">
+          <h2 className="text-2xl font-bold mb-4 text-accent_text_color">citizens of spokystan 🏆</h2>
+          {citizens.length > 0 ? (
+            <ul className="list-none pl-0">
+              {citizens.map((citizen, index) => (
+                <li key={index} className="text-accent_text_color">
+                  {'@' + citizen.username || citizen.name}
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <p className="text-subtitle_text_color">no citizens yet, be the first</p>
+          )}
+        </div>
       )}
     </div>
   );
