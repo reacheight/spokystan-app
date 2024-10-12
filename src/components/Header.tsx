502974afff4b8c3fb72c08c81d652443a435ad5f
Roman Mendaliev <reacheight8@gmail.com>
2024-10-12 18:35:49 +0300

    update logo and show it in header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,8 @@ import { TonConnectButton } from "@tonconnect/ui-react";
 
 const Header = () => {
   return (
-    <header className="flex justify-end p-4 z-10">
+    <header className="flex justify-between items-center p-4 z-10">
+      <img src="/logo.png" alt="Logo" className="h-10" />
       <TonConnectButton />
     </header>
   );
